b6521db08995c94b023e67735d6112d6c7e9d7b5
anna-ruiz <anna.ruiz.blazquez@cieep.com>
2023-09-19 14:26:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a03_ejemploactividades/SecondActivity.java
@@ -0,0 +1,52 @@
+package com.example.a03_ejemploactividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Log.e("ESTADOS","1- En el método onCreate SECOND");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("ESTADOS","2- En el método onStart del SECOND");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("ESTADOS","3- En el método onResume del SECOND");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("ESTADOS","4- En el método onPause del SECOND");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("ESTADOS","5- En el método onStop del SECOND");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("ESTADOS","6- En el método onRestart del SECOND");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("ESTADOS", "7- En el método onDestroy del SECOND");
+    }
+}
